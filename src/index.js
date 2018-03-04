ae52cb10794b22cc7dd5749dc775a7b5bb39c887
신창환 <changhwan-sin@nhn.com>
2018-03-04 13:52:09 +0900

    build 폴더만 푸시

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MovieList from './MovieList';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-
-ReactDOM.render(<MovieList />, document.getElementById('root'));
-registerServiceWorker();
